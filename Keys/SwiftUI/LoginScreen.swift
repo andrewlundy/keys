91789b8b27ab377fc3fe820750e5818818a08a3b
Andrew Lundy <andrewlundy@Andrews-iMac.local>
2019-07-20 12:48:30 -0500

    Update.

---
+++ Keys/SwiftUI/LoginScreen.swift
@@ -0,0 +1,23 @@
+//
+//  LoginScreen.swift
+//  Keys
+//
+//  Created by Andrew Lundy on 7/3/19.
+//  Copyright © 2019 Andrew Lundy. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginScreen : View {
+    var body: some View {
+        Text("Testing")
+    }
+}
+
+#if DEBUG
+struct LoginScreen_Previews : PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+    }
+}
+#endif
